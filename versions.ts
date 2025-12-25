b3f02387032af7400b96d9d99d3d23aafeb0ec56
chumer <unlitedfgoose@gmail.com>
2025-12-26 00:11:13 +0500

    Update versions.ts

--- versions.ts
+++ versions.ts
@@ -10,64 +10,78 @@ import { Version } from './types';
 export const GAME_VERSIONS: Version[] = [
   {
     id: '1',
+    version: '1.21.110',
+    releaseDate: '25.12.2025',
+    type: 'beta',
+    downloadUrl: 'https://drive.google.com/file/d/12kpYk3HmLb_23ppjAm7kr8FxJsFnqYCh/view?usp=sharing' 
+  },
+  {
+    id: '2',
     version: '1.21.90 beta 2',
-    releaseDate: 'N/A',
+    releaseDate: '13.07.2025',
     type: 'beta',
     downloadUrl: 'https://drive.google.com/file/d/1h_gx9VXX_0pzlAx6R1boSSZvJBJUQu_F/view?usp=sharing' 
   },
   {
-    id: '2',
+    id: '3',
     version: '1.21.90 beta 1',
-    releaseDate: 'N/A',
+    releaseDate: '26.06.2025',
     type: 'beta',
     downloadUrl: 'https://drive.google.com/file/d/1Zd95-l_qDCRBkWqZNAi_B33JShmoCMC9/view?usp=sharing'
   },
   {
-    id: '3',
+    id: '4',
     version: '1.21.50',
-    releaseDate: 'N/A',
+    releaseDate: '29.01.2025',
     type: 'stable',
     downloadUrl: 'https://drive.google.com/file/d/1KhjF8ZlGFPzcblKUD_YKyjP7mS1X1FJZ/view?usp=sharing'
   },
   {
-    id: '4',
+    id: '5',
     version: '1.20.10',
-    releaseDate: 'N/A',
+    releaseDate: '29.01.2025',
     type: 'stable',
     downloadUrl: 'https://drive.google.com/file/d/1v2lAnot5UTnfvmTbbLrFgh4ypvZ60UmT/view?usp=sharing'
   },
     {
-    id: '5',
+    id: '6',
     version: '1.19.52',
-    releaseDate: 'N/A',
+    releaseDate: '29.01.2025',
     type: 'stable',
     downloadUrl: 'https://drive.google.com/file/d/172YASqq8GdA_ZxaJ-mx2NbyCU_vqvha1/view?usp=sharing'
   },
       {
-    id: '6',
+    id: '7',
     version: '1.18.31',
-    releaseDate: 'N/A',
+    releaseDate: '29.01.2025',
     type: 'stable',
     downloadUrl: 'https://drive.google.com/file/d/1KK8Xt3i9w2zn_uh9DnAWESvcC5DiUdOd/view?usp=sharing'
   },
       {
-    id: '7',
+    id: '8',
     version: '1.17.30',
-    releaseDate: 'N/A',
+    releaseDate: '29.01.2025',
     type: 'stable',
     downloadUrl: 'https://drive.google.com/file/d/1RkMn2_qRWRzzYzSCltPyDh5qKlQ7Tzry/view?usp=sharing'
   },
+     {
+    id: '9',
+    version: '1.14.31',
+    releaseDate: '25.12.2025',
+    type: 'stable',
+    downloadUrl: 'https://drive.google.com/file/d/1Scl5kXJS3MzMvCy9ViE8KBiX5fKesP95/view?usp=sharing' 
+  },
       {
-    id: '8',
-    version: '1.120',
-    releaseDate: 'N/A',
+    id: '10',
+    version: '1.12.0',
+    releaseDate: '29.01.2025',
     type: 'stable',
     downloadUrl: 'https://drive.google.com/file/d/1ufioTVGrUP405M4HFurE-7FjAuyQkrM2/view?usp=sharing'
   },
       {
-    id: '9',
+    id: '11',
     version: '1.7.3.1',
-    releaseDate: 'N/A',
+    releaseDate: '29.01.2025',
     type: 'stable',
     downloadUrl: 'https://drive.google.com/file/d/1-ujgkCcfX07VXMR7PbURdgB2-l9jW66x/view?usp=sharing'
   },
